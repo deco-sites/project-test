bf81911835be9fdfa090b9b6ba90e9929e4157c6
Gabriel Collo <gcollo46@gmail.com>
2023-05-17 20:04:58 -0300

    feat: section informations

--- live.gen.ts
+++ live.gen.ts
@@ -24,12 +24,13 @@ import * as $$$$$$$$5 from "./sections/Features.tsx";
 import * as $$$$$$$$6 from "./sections/Footer.tsx";
 import * as $$$$$$$$7 from "./sections/Header.tsx";
 import * as $$$$$$$$8 from "./sections/Highlights.tsx";
-import * as $$$$$$$$9 from "./sections/LinkTree.tsx";
-import * as $$$$$$$$10 from "./sections/ProductDetails.tsx";
-import * as $$$$$$$$11 from "./sections/ProductShelf.tsx";
-import * as $$$$$$$$12 from "./sections/SearchResult.tsx";
-import * as $$$$$$$$13 from "./sections/WhatsApp.tsx";
-import * as $$$$$$$$14 from "./sections/WishlistGallery.tsx";
+import * as $$$$$$$$9 from "./sections/Informations.tsx";
+import * as $$$$$$$$10 from "./sections/LinkTree.tsx";
+import * as $$$$$$$$11 from "./sections/ProductDetails.tsx";
+import * as $$$$$$$$12 from "./sections/ProductShelf.tsx";
+import * as $$$$$$$$13 from "./sections/SearchResult.tsx";
+import * as $$$$$$$$14 from "./sections/WhatsApp.tsx";
+import * as $$$$$$$$15 from "./sections/WishlistGallery.tsx";
 import * as $live_middleware from "$live/routes/_middleware.ts";
 import * as $live_workbench from "$live/routes/live/workbench.ts";
 import * as $live_invoke from "$live/routes/live/invoke/index.ts";
@@ -176,12 +177,13 @@ const manifest = {
     "deco-sites/project-test/sections/Footer.tsx": $$$$$$$$6,
     "deco-sites/project-test/sections/Header.tsx": $$$$$$$$7,
     "deco-sites/project-test/sections/Highlights.tsx": $$$$$$$$8,
-    "deco-sites/project-test/sections/LinkTree.tsx": $$$$$$$$9,
-    "deco-sites/project-test/sections/ProductDetails.tsx": $$$$$$$$10,
-    "deco-sites/project-test/sections/ProductShelf.tsx": $$$$$$$$11,
-    "deco-sites/project-test/sections/SearchResult.tsx": $$$$$$$$12,
-    "deco-sites/project-test/sections/WhatsApp.tsx": $$$$$$$$13,
-    "deco-sites/project-test/sections/WishlistGallery.tsx": $$$$$$$$14,
+    "deco-sites/project-test/sections/Informations.tsx": $$$$$$$$9,
+    "deco-sites/project-test/sections/LinkTree.tsx": $$$$$$$$10,
+    "deco-sites/project-test/sections/ProductDetails.tsx": $$$$$$$$11,
+    "deco-sites/project-test/sections/ProductShelf.tsx": $$$$$$$$12,
+    "deco-sites/project-test/sections/SearchResult.tsx": $$$$$$$$13,
+    "deco-sites/project-test/sections/WhatsApp.tsx": $$$$$$$$14,
+    "deco-sites/project-test/sections/WishlistGallery.tsx": $$$$$$$$15,
     "deco-sites/std/sections/Analytics.tsx": i2$$$$3,
     "deco-sites/std/sections/configButterCMS.global.tsx": i2$$$$4,
     "deco-sites/std/sections/configOCC.global.tsx": i2$$$$5,
---
+++ sections/Informations.tsx
@@ -0,0 +1,11 @@
+export interface Props {
+  title: string;
+}
+
+export default function Informations({ title }: Props){
+  return (
+    <div class="bg-red-900 w-full text-center p-2">
+      <span class="text-white text-lg font-bold">{title}</span>
+    </div>
+  );
+}
