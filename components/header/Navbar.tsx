c4c666afe79a5c4edc01ea677ea71edd94b8aa13
Gabriel Collo <gcollo46@gmail.com>
2023-05-21 03:17:44 -0300

    fix: header responsive

--- components/header/Navbar.tsx
+++ components/header/Navbar.tsx
@@ -18,7 +18,7 @@ function Navbar({ items, searchbar, itemsSecondary }: {
       {/* Mobile Version */}
       <div
         style={{ height: navbarHeight }}
-        class="md:hidden grid grid-cols-[2fr_1fr_2fr] border-b border-base-200 w-full pl-2 pr-6 gap-2"
+        class="lg:hidden grid grid-cols-[2fr_1fr_2fr] border-b border-base-200 w-full pl-2 pr-6 gap-2"
       >
         <div class="flex justify-start items-center gap-3">
           <Buttons variant="menu" />
@@ -59,7 +59,7 @@ function Navbar({ items, searchbar, itemsSecondary }: {
       </div>
 
       {/* Desktop Version */}
-      <div class="hidden md:flex flex-row justify-between items-center border-b border-base-200 w-full pl-2 pr-6">
+      <div class="hidden lg:flex flex-row justify-between items-center border-b border-base-200 w-full pl-2 pr-6">
         <div class="flex-none w-44">
           <a href="/" aria-label="Store logo" class="block px-4 py-3 w-[160px]">
             <Icon id="Logo" width={180} height={40} />
