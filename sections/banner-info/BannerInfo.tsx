bca30e56b991752ac8ab98322bb73e8c54375c0f
Rafael_Oliveira7 <rafael.miranda@rocky.ag>
2023-05-21 01:54:42 -0300

    fix: pagespeed agora vai

--- sections/banner-info/BannerInfo.tsx
+++ sections/banner-info/BannerInfo.tsx
@@ -1,6 +1,6 @@
 import { Picture, Source } from "deco-sites/std/components/Picture.tsx";
 import type { Image as LiveImage } from "deco-sites/std/components/types.ts";
-import Image from "deco-sites/std/components/Image.tsx"
+import Image from "deco-sites/std/components/Image.tsx";
 
 export interface Banner {
   srcMobile: LiveImage;
@@ -32,22 +32,22 @@ export interface Props {
 }
 
 const COLORTEXT = {
-    "Black": "text-black",
-    "White": "text-white",
-}
+  "Black": "text-black",
+  "White": "text-white",
+};
 const COLOR_BUTTON_TEXT = {
-    "Black": "text-white",
-    "White": "text-black",
-}
+  "Black": "text-white",
+  "White": "text-black",
+};
 const COLOR_BUTTON = {
-    "Black": "bg-black after:border-black",
-    "White": "bg-white after:border-white",
-}
-const POSITION_INFO ={
+  "Black": "bg-black after:border-black",
+  "White": "bg-white after:border-white",
+};
+const POSITION_INFO = {
   "bottom": "md:bottom-[15%] xl:bottom-[40%]",
   "bottom-mid": "md:bottom-[30%]",
-  "mid": "md:bottom-[40%]"
-}
+  "mid": "md:bottom-[40%]",
+};
 export default function BannnerInfo({
   title,
   description,
@@ -60,28 +60,61 @@ export default function BannnerInfo({
 }: Props) {
   return (
     <section class="w-full relative">
-        <Picture>
-            <Source
-            media="(max-width: 767px)"
-            src={banner.srcMobile}
-            width={500}
-            />
-            <Source
-            media="(min-width: 768px)"
-            src={banner.srcDesktop ? banner.srcDesktop : banner.srcMobile}
-            width={1920}
-            />
-            <Image  width={250} height={250} src={banner.srcMobile} alt={banner.alt} loading="eager" decoding="sync" fetchPriority="high" preload/>
-        </Picture>
-        <div class={`absolute w-[90%] flex flex-col bottom-8 ${POSITION_INFO[positionText.desktop ?? "bottom"]} px-4 md:w-[45%] lg:pl-20`}>
-            <h2 class={`text-3xl ${COLORTEXT[colorText ?? "Black"]} font-semibold uppercase`}>{title}</h2>
-            <p class={`text-lg ${COLORTEXT[colorText ?? "Black"]}`}>{description}</p>
-            <a href={`${href}`} class={`relative z-10 flex justify-between items-center pl-2 ${COLOR_BUTTON_TEXT[colorButon ?? "Black"]} w-52 h-12 mt-1 after:content-[''] after:z-[-1] after:block after:w-full after:h-12 ${COLOR_BUTTON[colorButon ?? "Black"]} after:border-2 after:absolute after:top-1 after:left-1`}>
-                {labelButton}
-                <span class={`mr-3 text-3xl ${COLOR_BUTTON_TEXT[colorButon ?? "Black"]} pb-1`}>→</span>    
-            </a>
-        </div>
-        
+      <Picture>
+        <Source
+          media="(max-width: 767px)"
+          src={banner.srcMobile}
+          width={500}
+        />
+        <Source
+          media="(min-width: 768px)"
+          src={banner.srcDesktop ? banner.srcDesktop : banner.srcMobile}
+          width={1920}
+        />
+        <Image
+          width={250}
+          height={250}
+          src={banner.srcMobile}
+          alt={banner.alt}
+          loading="eager"
+          decoding="sync"
+          fetchPriority="high"
+          preload
+        />
+      </Picture>
+      <div
+        class={`absolute w-[90%] flex flex-col bottom-8 ${
+          POSITION_INFO[positionText.desktop ?? "bottom"]
+        } px-4 md:w-[45%] lg:pl-20`}
+      >
+        <h2
+          class={`text-3xl ${
+            COLORTEXT[colorText ?? "Black"]
+          } font-semibold uppercase`}
+        >
+          {title}
+        </h2>
+        <p class={`text-lg ${COLORTEXT[colorText ?? "Black"]}`}>
+          {description}
+        </p>
+        <a
+          href={`${href}`}
+          class={`relative z-10 flex justify-between items-center pl-2 ${
+            COLOR_BUTTON_TEXT[colorButon ?? "Black"]
+          } w-52 h-12 mt-1 after:content-[''] after:z-[-1] after:block after:w-full after:h-12 ${
+            COLOR_BUTTON[colorButon ?? "Black"]
+          } after:border-2 after:absolute after:top-1 after:left-1`}
+        >
+          {labelButton}
+          <span
+            class={`mr-3 text-3xl ${
+              COLOR_BUTTON_TEXT[colorButon ?? "Black"]
+            } pb-1`}
+          >
+            →
+          </span>
+        </a>
+      </div>
     </section>
   );
 }
